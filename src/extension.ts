189699f30ac97b5f2014dba76aedc7350d2497cd
Eam <eam.doherty@gmail.com>
2024-03-19 20:25:42 -0700

    Add commands for jumping to line and selecting lines by number.

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,57 @@ import * as vscode from 'vscode';
 import * as commandRunner from "./command_runner";
 
 
+// Jumps to the given line. Input line numbers are 1-based.
+function jumpToLine(line: number) {
+  console.log(`Jumping to line: ${line}`);
+
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  // Line numbers are 0-based in the API.
+  const documentLine = editor.document.lineAt(line - 1);
+  if (!documentLine) {
+    throw Error(`Invalid line: ${line}`);
+  }
+
+  const position = new vscode.Position(
+    documentLine.lineNumber,
+    documentLine.firstNonWhitespaceCharacterIndex
+  );
+  const selection = new vscode.Selection(position, position);
+  editor.selection = selection;
+  editor.revealRange(selection);
+
+  console.log(`Jumped to line: ${line}`);
+}
+
+// Selects a range of lines. Input line numbers are 1-based.
+// If lineTo is not provided, only lineFrom will be selected.
+function selectLineRange(lineFrom: number, lineTo?: number) {
+  const effectiveLineTo = lineTo || lineFrom;
+  console.log(`Selecting lines: ${lineFrom} to ${effectiveLineTo}`);
+
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  // Line numbers are 0-based in the API.
+  // Select from start of line to start of subsequent line.
+  const document = editor.document;
+  const start = document.lineAt(lineFrom - 1).range.start;
+  const end = effectiveLineTo >= editor.document.lineCount - 1 ?
+    document.lineAt(editor.document.lineCount - 1).range.end : document.lineAt(effectiveLineTo).range.start;
+
+  const selection = new vscode.Selection(start, end);
+  editor.selection = selection;
+  editor.revealRange(selection);
+
+  console.log(`Selected lines: ${lineFrom} to ${effectiveLineTo}`);
+}
+
 // Function called when the extension activates.
 // This extension should activate immediately when visual studios opens so it can create the communication directory
 // if necessary.
@@ -13,7 +64,11 @@ export async function activate(context: vscode.ExtensionContext) {
   await commandRunner.initialize();
 
   // Define available commands.
-  context.subscriptions.push(vscode.commands.registerCommand('eam-talon.runCommand', commandRunner.runCommand));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('eam-talon.runCommand', commandRunner.runCommand),
+    vscode.commands.registerCommand('eam-talon.jumpToLine', jumpToLine),
+    vscode.commands.registerCommand('eam-talon.selectLineRange', selectLineRange)
+  );
 }
 
 // Function called when the extension is deactivated.
