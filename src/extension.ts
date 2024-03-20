e7dc09b2926901de721f87d88edbb9613a1e5b58
Eam <eam.doherty@gmail.com>
2024-03-19 20:34:56 -0700

    Add a command for copying a range of lines to the cursor.

--- src/extension.ts
+++ src/extension.ts
@@ -54,6 +54,36 @@ function selectLineRange(lineFrom: number, lineTo?: number) {
   console.log(`Selected lines: ${lineFrom} to ${effectiveLineTo}`);
 }
 
+// Copies the contents of a range of lines to the cursor location. Input line numbers are 1-based.
+// If lineTo is not provided, only lineFrom will be copied.
+function copyLinesToCursor(lineFrom: number, lineTo?: number) {
+  const effectiveLineTo = lineTo || lineFrom;
+  console.log(`Copying lines: ${lineFrom} to ${effectiveLineTo}`);
+
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+
+  // Line numbers are 0-based in the API.
+  const document = editor.document;
+  const start = document.lineAt(lineFrom - 1).range.start;
+  const end = effectiveLineTo >= editor.document.lineCount - 1 ?
+    document.lineAt(editor.document.lineCount - 1).range.end : document.lineAt(effectiveLineTo).range.start;
+
+  const text = document.getText(new vscode.Range(start, end));
+
+  // Insert the text at the start of the line the cursor is on.
+  const cursorLine = editor.document.lineAt(editor.selection.active.line);
+  const cursorPosition = cursorLine.range.start;
+  editor.edit(builder => {
+    builder.insert(cursorPosition, text);
+  });
+
+  console.log(`Copied lines: ${lineFrom} to ${effectiveLineTo}`);
+}
+
+
 // Function called when the extension activates.
 // This extension should activate immediately when visual studios opens so it can create the communication directory
 // if necessary.
@@ -67,7 +97,8 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('eam-talon.runCommand', commandRunner.runCommand),
     vscode.commands.registerCommand('eam-talon.jumpToLine', jumpToLine),
-    vscode.commands.registerCommand('eam-talon.selectLineRange', selectLineRange)
+    vscode.commands.registerCommand('eam-talon.selectLineRange', selectLineRange),
+    vscode.commands.registerCommand('eam-talon.copyLinesToCursor', copyLinesToCursor)
   );
 }
 
